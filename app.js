e796a2d3a79fdd2fac5766bcdb9ce4ef643dcc47
memetibryan <memetibryan@gmail.com>
2019-10-14 17:56:39 +0300

    removed comment brackets

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('./index/index.html');
+  res.end './index/index.html';
 });
 
 server.listen(port,() => {
