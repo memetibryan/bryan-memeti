fcbb5fc4d500ec26c83ebe0e661df053748de69e
memetibryan <memetibryan@gmail.com>
2019-10-14 18:07:35 +0300

    specified folder

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   // res.end('<h1>Hello World</h1>');
-  "index.html"
+  "./index/index.html"
 });
 
 server.listen(port,() => {
